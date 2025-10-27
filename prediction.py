a778ea6377d10c1be5ab62ea4f14a0a15b57f01a
Manvi0408 <manviitnd0408@gmail.com>
2025-10-27 07:16:45 +0530

    Uploaded Fake News Detection project files

--- prediction.py
+++ prediction.py
@@ -1,28 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec  4 17:45:40 2017
-
-@author: NishitP
+Updated prediction.py to work with current scikit-learn
 """
 
-import pickle
+from joblib import load
 
-#doc_new = ['obama is running for president in 2016']
+# Load the trained model and vectorizer
+model = load('final_model.joblib')
+vectorizer = load('tfidf_vectorizer.joblib')
 
-var = input("Please enter the news text you want to verify: ")
-print("You entered: " + str(var))
+# Function to predict fake news
+def detecting_fake_news(var):
+    vect_text = vectorizer.transform([var])
+    prediction = model.predict(vect_text)
+    prob = model.predict_proba(vect_text)
+    
+    print("The given statement is:", prediction[0])
+    print("The truth probability score is:", prob[0][1])
 
-
-#function to run for prediction
-def detecting_fake_news(var):    
-#retrieving the best model for prediction call
-    load_model = pickle.load(open('final_model.sav', 'rb'))
-    prediction = load_model.predict([var])
-    prob = load_model.predict_proba([var])
-
-    return (print("The given statement is ",prediction[0]),
-        print("The truth probability score is ",prob[0][1]))
-
-
+# Main block
 if __name__ == '__main__':
+    var = input("Please enter the news text you want to verify: ")
+    print("You entered:", var)
     detecting_fake_news(var)
